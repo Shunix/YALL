0ee7f7832de35100b9d17a0fc135f4dee7425893
Shunix <admin@shunix.com>
2016-04-17 21:18:36 +0800

    Flush the content to file

--- yall/src/main/java/com/shunix/yall/Yall.java
+++ yall/src/main/java/com/shunix/yall/Yall.java
@@ -57,6 +57,8 @@ public class Yall implements YallConfig {
                         break;
                     }
                 }
+                fileWriter.flush();
+                writer.flush();
             } catch (Exception e) {
 
             } finally {
